14c140df486a26efd3aad4e47329d9cc2720d6ce
nowayitsaj <creepercreep07@gmail.com>
2024-02-18 20:47:52 -0600

    finish 6.45

---
+++ Chapter 6/6.45 LAB.cpp
@@ -0,0 +1,31 @@
+// Fibonacci sequence
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int Fibonacci(int n) {
+   
+    if (n > -1) {
+
+        vector<int> sequence = {0, 1};
+
+        for (int i = 1; i < n; i++) {
+            sequence.push_back(sequence[i - 1] + sequence[i]);       
+        }
+
+        return sequence.at(n);
+    }
+
+    return -1;
+
+}
+
+int main() {
+    int startNum;
+        
+    cin >> startNum;
+    cout << "Fibonacci(" << startNum << ") is " << Fibonacci(startNum) << endl;
+
+    return 0;
+}
